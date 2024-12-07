865b8c3b9db0bedb6eaa5ca14aa2051eac06fe4a
Amnezou Lahcen <amnezoulahcen@gmail.com>
2024-12-07 21:47:04 +0100

    commit

--- conf.py
+++ conf.py
@@ -7,8 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Plastic Waste detection and tracking'
-copyright = '2024, GIIADS, by AMNEZOU LAHCEN '
-author = 'AMNEZOU LAHCEN'
+copyright = '2024, GIIADS, by AMNEZOU LAHCEN & SALMA BOURKIBA'
+author ='AMNEZOU LAHCEN'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
