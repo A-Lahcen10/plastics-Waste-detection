25f28d77e0ce97e977b9233fd00792f548b4cc23
Amnezou Lahcen <amnezoulahcen@gmail.com>
2024-12-28 10:44:57 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Our first Github Project 
-Plastic Waste Detection
+Plastic Waste Detection and tracking 
+
+Pour un guide plus détaillé sur notre projet , veuillez vous référer à la documentation officielle sur **ReadTheDocs**.
+
+Vous pouvez accéder à la documentation [ici](https://plastics-waste-detection-and-tracking.readthedocs.io/en/latest/).
--- tracking.py
+++ tracking.py
@@ -1,12 +1,14 @@
 import cv2
 from ultralytics import YOLO
 # Load the YOLO model
-model = YOLO('model.pt')
+model = YOLO('plastics-Waste-detection-and-Tracking/best_final.pt')
 
 class_list = model.names
 
 # Open the video file
-cap = cv2.VideoCapture('video4.mp4')
+cap = cv2.VideoCapture(0)
+
+
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -14,15 +16,19 @@ while cap.isOpened():
         break
     results = model.track(frame, persist=True) 
 
-    # Ensure results are not empty
+    
     if results[0].boxes.data is not None:
-        # Get the detected boxes, their class indices, and track IDs
+   
         boxes = results[0].boxes.xyxy
-        track_ids = results[0].boxes.id.int()
+        if results[0].boxes.data is not None:
+        
+            if results[0].boxes.id is not None:
+                track_ids = results[0].boxes.id.int()
+            else:
+                track_ids = []
         class_indices = results[0].boxes.cls.int()
         confidences = results[0].boxes.conf
 
-        # Loop through each detected object
         for box, track_id, class_idx, conf in zip(boxes, track_ids, class_indices, confidences):
             x1, y1, x2, y2 = map(int, box)
                 
@@ -30,18 +36,11 @@ while cap.isOpened():
             
             cv2.putText(frame, f"ID: {track_id} {class_name}", (x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) 
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-
-    
-    
-    # Show the frame
     cv2.imshow(" Object Tracking", frame)    
     
     # Exit loop if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        break            
 
-# Release resources
-cap.release()
-cv2.destroyAllWindows()
